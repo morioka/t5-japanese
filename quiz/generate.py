6e8cc97f5ff86f8411944393d76c3007b81f9ba7
Yasuhiro Morioka <ymorioka@gmail.com>
2023-03-24 22:57:38 +0900

    add generate_type switch to generate.py

--- quiz/generate.py
+++ quiz/generate.py
@@ -7,6 +7,7 @@ from transformers import pipeline
 
 import unicodedata
 import neologdn
+import mojimoji
 
 from classopt import classopt
 
@@ -28,10 +29,13 @@ class Args:
 
     answer: str = "富士山"
     context: str = "富士山は静岡県と山梨県にまたがっている山です。"
+    question: str = "静岡県と山梨県にはどんな山がありますか?"
 
     bad_words: list[str] = None
 
     verbose: bool = False               # デバグ用：引数を出力
+
+    generate_type: str = "qa"   # 'qa' (Question Answering), AQG (Answer-aware Question Generation), AQG-HL (AQG with higlights)
 
 
 def normalize_text(text):
@@ -41,8 +45,12 @@ def normalize_text(text):
     assert "\n" not in text
     assert len(text) > 0
 
+    text = text.replace("\u3000", " ").replace("\n", " ")
+    text = mojimoji.zen_to_han(text, kana=False)  # 英数字を半角に
+    text = mojimoji.han_to_zen(text, ascii=False, digit=False)  # かなを全角に
+
     text = neologdn.normalize(unicodedata.normalize('NFKC', text))
-    #text = text.lower()
+    text = text.lower() # t5-japaneseは英小文字 uncased らしい
     return text
 
 
@@ -64,16 +72,22 @@ def main():
     # question = "静岡県と山梨県にはどんな山がありますか?"
     answer = conf.answer
     context = conf.context
+    question = conf.question
 
     # 前処理
     answer = normalize_text(answer.replace("\n", " "))
     context = normalize_text(context.replace("\n", " "))
+    question = normalize_text(question.replace("\n", " "))
 
     # 乱数シード初期化
     set_seed(conf.seed)
 
     # プロンプト
-    text2text_question_generaton_prompt = "answer: {answer} context: {context}"
+    text2text_generaton_prompt = "question: {question} context: {context}"
+    if conf.generate_type == 'aqg':
+        text2text_generaton_prompt = "answer: {answer} context: {context}"
+    if conf.generate_type == 'aqg-hl':
+        text2text_generaton_prompt = "{context}"
 
     # モデルとパイプラインの初期化
     text2text_generator = pipeline("text2text-generation",
@@ -89,7 +103,7 @@ def main():
 
     # 生成
     generated = text2text_generator(
-                    text2text_question_generaton_prompt.format(answer=answer, context=context),
+                    text2text_generaton_prompt.format(answer=answer, question=question, context=context),
                     # 以下、 generate() への入力
                     max_length=conf.max_target_length ,
                     temperature=conf.temperature,
@@ -108,19 +122,33 @@ if __name__ == "__main__":
 
 
 """
-$ python predict.py --verbose
+$ python generate.py --verbose  --generate_typ aqg
 Args(model_name_or_path='sonoisa/t5-base-japanese-question-generation', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='富士山', context='富士山は静岡県と山梨県にまたがっている山です。', bad_words=None)
 静岡県と山梨県にはどんな山がありますか?
-$ python predict.py --verbose --answer 山梨県
+$ python generate.py --verbose --answer 山梨県  --generate_typ aqg
 Args(model_name_or_path='sonoisa/t5-base-japanese-question-generation', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='山梨県', context='富士山は静岡県と山梨県にまたがっている山です。', bad_words=None)
 静岡県と他のどの県にまたがっている富士山ですか?
-$ python predict.py --verbose --answer 山梨県 --context 山梨県の県庁所在地は甲府市です。
+$ python generate.py --verbose --answer 山梨県 --context 山梨県の県庁所在地は甲府市です。  --generate_typ aqg
 Args(model_name_or_path='sonoisa/t5-base-japanese-question-generation', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='山梨県', context='山梨県の県庁所在地は甲府市です。', bad_words=None)
 どの県が県庁所在地ですか
-$ python predict.py --verbose --answer 甲府市 --context 山梨県の県庁所在地は甲府市です。
+$ python generate.py --verbose --answer 甲府市 --context 山梨県の県庁所在地は甲府市です。  --generate_typ aqg
 Args(model_name_or_path='sonoisa/t5-base-japanese-question-generation', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='甲府市', context='山梨県の県庁所在地は甲府市です。', bad_words=None)
 山梨県の県庁所在地はどこですか?
-$ python predict.py --verbose --answer 甲府市 --context 山梨県の県庁所在地は甲府市です。 --bad_words 山梨 山梨県
+$ python generate.py --verbose --answer 甲府市 --context 山梨県の県庁所在地は甲府市です。 --bad_words 山梨 山梨県  --generate_typ aqg
 Args(model_name_or_path='sonoisa/t5-base-japanese-question-generation', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='甲府市', context='山梨県の県庁所在地は甲府市です。', bad_words=['山梨', '山梨県'])
 中部地方の県庁所在地はどこですか?
+"""
+
+"""
+$ python generate.py --verbose --question 山梨県の県庁所在地は? --context 山梨県の県庁所在地は甲府市です。 --model_name_or_path ../../../t5-japanese/model_t5_JSQUAD_MRC --generate_typ qa
+Args(model_name_or_path='../../../t5-japanese/model_t5_JSQUAD_MRC', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='富士山', context='山梨県の県庁所在地は甲府市です。', question='山梨県の県庁所在地は?', bad_words=None, verbose=True, generate_type='qa')
+甲府市
+
+$ python generate_question_answering.py --verbose --answer 甲府市 --question 山梨県の県庁所在地は? --context 山梨県の県庁所在地は甲府市です。 --model_name_or_path sonoisa/t5-base-japanese-question-generation --generate_typ aqg
+Args(model_name_or_path='sonoisa/t5-base-japanese-question-generation', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='甲府市', context='山梨県の県庁所在地は甲府市です。', question='山梨県の県庁所在地は?', bad_words=None, verbose=True, generate_type='aqg')
+山梨県の県庁所在地はどこですか?
+
+$ python generate.py --verbose --answer 甲府市 --question 山梨県の県庁所在地は? --context 山梨県の県庁所在地は\<hl\>甲府市\<hl\>です。 --model_name_or_path ../../../t5-japanese/model_t5_JSQUAD_MRC --generate_typ aqg-hl
+Args(model_name_or_path='../../../t5-japanese/model_t5_JSQUAD_MRC', tokenizer_name_or_path=None, seed=42, max_input_length=512, max_target_length=64, temperature=1.0, repetition_penalty=1.5, num_beams=10, diversity_penalty=1.0, num_beam_groups=10, num_return_sequences=10, answer='甲府市', context='山梨県の県庁所在地は<hl>甲府市<hl>です。', question='山梨県の県庁所在地は?', bad_words=None, verbose=True, generate_type='aqg-hl')
+甲府
 """
