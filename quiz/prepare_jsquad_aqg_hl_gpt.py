145526675abd5934459e10c1ad837a7949b146f1
Yasuhiro Morioka <ymorioka@gmail.com>
2023-03-27 23:41:30 +0900

    update prepare_jsquad_aqg_gpt

--- quiz/prepare_jsquad_aqg_hl_gpt.py
+++ quiz/prepare_jsquad_aqg_hl_gpt.py
@@ -15,3 +15,21 @@ for data_file in ['train', 'dev', 'test']:
         for _, row in df.iterrows():
             _, i, o = row
             f.write(f'<s>{i}[SEP]{o}</s>\n')
+
+
+##
+
+dir_in = "data_jsquad_aqg"
+dir_out = "data_jsquad_aqg_gpt"
+
+assert dir_in != dir_out
+
+os.makedirs(dir_out, exist_ok=True)
+
+for data_file in ['train', 'dev', 'test']:
+    df = pd.read_csv(f'{dir_in}/{data_file}.tsv', sep='\t')
+
+    with open(f'{dir_out}/{data_file}.txt', 'w') as f:
+        for _, row in df.iterrows():
+            _, i, o = row
+            f.write(f'<s>{i}[SEP]{o}</s>\n')
